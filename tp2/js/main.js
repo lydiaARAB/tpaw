7a815cd66fd9856b54f9afdbb41a7601ea424f2a
lydiaARAB <44161494+lydiaARAB@users.noreply.github.com>
2018-11-05 18:10:57 +0100

    ok
    okv

--- tp2/js/main.js
+++ tp2/js/main.js
@@ -14,7 +14,7 @@ function validation(){
     {
         var d= document.querySelector("#resultat");
         d.style.backgroundColor='rgb(0, 128, 0)';
-        d.style.padding='5px';
+        d.style.padding='3px';
         var t=  document.getElementById("txt");
         t.style.color="rgb(255, 255, 255)";
         t.innerHTML="Bienvenue"+" "+document.querySelector("#prenom").value; 
@@ -45,63 +45,6 @@ function validation(){
     
     }
 }
-
-/*
-if (nom<5) {
-    
-    var d= document.querySelector("#error1");
-    d.style.backgroundColor='rgb(182, 82, 137)';
-    d.style.padding='5px';
-    d.style.border='1px black';
-    document.getElementById("p1").innerHTML="la saisie du nom est obligatoire ! "
-    
-} else {
-    if (prenom<5) {
-        var d= document.querySelector("#error2");
-        d.style.backgroundColor='rgb(182, 82, 137)';
-        d.style.padding='5px';
-        d.style.border='1px black';
-        document.getElementById("p2").innerHTML="la saisie du prenom est obligatoire ! ";
-        
-    } else {
-        if (adresse<5) {
-            var d= document.querySelector("#error3");
-            d.style.backgroundColor='rgb(182, 82, 137)';
-            d.style.padding='5px';
-            d.style.border='1px black';
-            document.getElementById("p3").innerHTML="la saisie  d'adresse est obligatoire ! "
-    } else {
-        if (mail<5) {
-            var d= document.querySelector("#error4");
-            d.style.backgroundColor='rgb(182, 82, 137)';
-            d.style.padding='5px';
-            d.style.border='1px black';
-            document.getElementById("p4").innerHTML="la saisie de mail est obligatoire ! "
-    
-        } else {
-            if (date_naissance<5) {
-                var d= document.querySelector("#error5");
-                d.style.backgroundColor='rgb(182, 82, 137)';
-                d.style.padding='5px';
-                d.style.border='1px black';
-                document.getElementById("p5").innerHTML="la saisie de la date de naissancee est obligatoire ! ";
-            } else {
-                var d= document.querySelector("#resultat");
-                d.style.backgroundColor='rgb(0, 128, 0)';
-                d.style.padding='5px';
-                var t=  document.getElementById("txt");
-                t.style.color="rgb(255, 255, 255)";
-                t.innerHTML="Bienvenue"+" "+document.querySelector("#prenom").value; 
-            }
-            
-        }
-        
-    }
-        
-    }
-    
-}
-*/
 
 
 
