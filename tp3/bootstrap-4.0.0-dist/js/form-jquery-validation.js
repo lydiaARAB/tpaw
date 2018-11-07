969c80d52374000894d96b74eb7bdceed64d2056
lydiaARAB <44161494+lydiaARAB@users.noreply.github.com>
2018-11-08 00:36:53 +0100

    ok

--- tp3/bootstrap-4.0.0-dist/js/form-jquery-validation.js
+++ tp3/bootstrap-4.0.0-dist/js/form-jquery-validation.js
@@ -1,32 +1,38 @@
 $( document ).ready(function() {
     // ce code est exécuter une fois que toute la page est téléchargée par le navigateur
     // voir plus : https://www.w3schools.com/js/js_htmldom.asp
-     console.log( "DOM ready!" );
-
      //Déclarer les variables : 
-    var $nom=$("#nom"),
-        $prenom=$("#prenom"),
-        $date_nais=$("#date_nais"),
-        $adresse=$("#adresse"),
-        $email=$("#email"),
-        $champ=$(".champ");
-
+    
+       
+     $("#submit").on("click",function(event){ 
+       //Récuperer tous les chmaps de formulaires 
+      var  $nom=$("#nom"),
+           $prenom=$("#prenom"),
+           $date_nais=$("#date_nais"),
+           $adresse=$("#adresse"),
+           $email=$("#email");
+          
+    
+      if(verifier($nom)&& verifier($prenom) &&verifier($date_nais)&&
+      verifier($adresse) &&verifier($email))
+      {
+        $('#myModel').modal('show');
+      } 
+      //cette fonction permet de verifier si tous les champs sont valide
+      function verifier(_champ){
+        if(_champ.val()=="") {
+         return false
         
-    $champ.keyup(function(){
-        if($(this).val().length < 5){ // si la chaîne de caractères est inférieure à 5
-            $(this).css({ // on rend le champ rouge
-                borderColor : 'red',
-	        color : 'red'
-            });
-         }
-         else{
-             $(this).css({ // si tout est bon, on le rend vert
-	         borderColor : 'green',
-	         color : 'green'
-	     });
-         }
-    });
-
+        }
+        else {
+          return true; 
+        }
+      }
+      });
+      
      
-     // Y mettre le code jQuery pour valider tous les champs du formulaire
- });
+
+       
+        
+        });
+        
