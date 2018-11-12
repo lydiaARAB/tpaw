da300d824d24e977cb7f7cb7394ad535df760392
lydiaARAB <44161494+lydiaARAB@users.noreply.github.com>
2018-11-12 23:23:17 +0100

    ok
    ok

--- tp2/js/main.js
+++ tp2/js/main.js
@@ -8,9 +8,9 @@ function validation(){
 
     if( validateItem("nom", "error1")    && 
         validateItem("prenom", "error2") &&
-        validateItem("adresse","error3")  &&
-        validateItem("mail","error4")    &&
-        validateItem("date_naissance","error5") )
+        validateItem("date_naissance","error3")&&
+        validateItem("adresse","error4")  &&
+        validateItem("mail","error5") )
     {
         var d= document.querySelector("#resultat");
         d.style.backgroundColor='rgb(0, 128, 0)';
@@ -18,26 +18,27 @@ function validation(){
         d.style.color="rgb(255, 255, 255)";
         d.innerHTML="Bienvenue"+" "+document.querySelector("#prenom").value; 
     }
-
-
-
+  
     function validateItem(_id, _idError){
-
-        
         var el = document.querySelector("#"+_id);
         var label = document.querySelector('[for="'+_id+'"]');
         var error= document.querySelector("#"+_idError);
-        if (el.value.length<5) {
-            
+        var d1= document.querySelector("#resultat");
+
+        if (el.value.length<5) { 
+        
+            d1.innerHTML="";
+            d1.style.backgroundColor="#e9ebee"
             error.style.backgroundColor='rgb(182, 82, 137)';
             error.style.padding='5px';
             error.style.border='1px black';
             error.innerHTML="la saisie de "+ label.textContent +" est obligatoire ! ";
             return false;
         } else { 
+            
            error.style.backgroundColor=" #e9ebee";
            error.innerHTML="";
-            return true;
+           return true;
         }
     
     }
